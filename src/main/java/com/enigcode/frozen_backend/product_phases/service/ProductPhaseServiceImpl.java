27fdfb09119fd466da276322fb6ee03b70682a1a
FranSaMa373 <sanmartinfrancisco373@gmail.com>
2025-10-13 00:37:25 -0300

    SCRUM-49 Recetas y fases: Creacion de Repository Service,Controller, Mapper y DTOs para recetas y fases del producto

--- src/main/java/com/enigcode/frozen_backend/product_phases/service/ProductPhaseServiceImpl.java
+++ src/main/java/com/enigcode/frozen_backend/product_phases/service/ProductPhaseServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProductPhaseServiceImpl {
+public class ProductPhaseServiceImpl implements ProductPhaseService {
 
     final ProductPhaseRepository productPhaseRepository;
     final ProductRepository productRepository;
