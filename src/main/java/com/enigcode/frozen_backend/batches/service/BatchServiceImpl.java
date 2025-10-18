0ce55b9c24c01619f50685d5b3effa245bbe9f0f
Augusto Fuertes <augustofuertes11@gmail.com>
2025-10-17 22:11:08 -0300

    implementacion de funcion createBatch

--- src/main/java/com/enigcode/frozen_backend/batches/service/BatchServiceImpl.java
+++ src/main/java/com/enigcode/frozen_backend/batches/service/BatchServiceImpl.java
@@ -2,8 +2,14 @@ package com.enigcode.frozen_backend.batches.service;
 
 import com.enigcode.frozen_backend.batches.mapper.BatchMapper;
 import com.enigcode.frozen_backend.batches.model.Batch;
+import com.enigcode.frozen_backend.batches.model.BatchStatus;
 import com.enigcode.frozen_backend.batches.repository.BatchRepository;
+import com.enigcode.frozen_backend.common.exceptions_configs.exceptions.ResourceNotFoundException;
+import com.enigcode.frozen_backend.packagings.model.Packaging;
+import com.enigcode.frozen_backend.packagings.repository.PackagingRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -14,9 +20,30 @@ public class BatchServiceImpl implements BatchService{
 
     final BatchRepository batchRepository;
     final BatchMapper batchMapper;
+    final PackagingRepository packagingRepository;
 
     @Override
     public Batch createBatch(Long packagingId, OffsetDateTime plannedDate, Double orderQuantity) {
-        return null;
+        Packaging packaging = packagingRepository.findById(packagingId)
+            .orElseThrow(() -> new ResourceNotFoundException("No se encontro un paquete con el id " + packagingId));
+
+        Integer quantityInteger = (int) Math.floor(orderQuantity/packaging.getQuantity());
+
+        Batch batch = Batch.builder()
+                        .packaging(packaging)
+                        .status(BatchStatus.PENDIENTE)
+                        .quantity(quantityInteger)
+                        .plannedDate(plannedDate)
+                        .creationDate(OffsetDateTime.now())
+                        .startDate(null)
+                        .completedDate(null)
+                        .build();
+        
+        return batch;
     }
+
+     private String generateCode(Batch batch) {
+        return "LOT-" + batch.getId();
+    }
+
 }
