4eef52ff97ab478c69201b5db53acf78a28a24a1
Julian <julianchocobar@hotmail.com>
2025-11-21 12:57:17 -0300

    import no usado

--- src/main/java/com/enigcode/frozen_backend/analytics/controller/AnalyticsController.java
+++ src/main/java/com/enigcode/frozen_backend/analytics/controller/AnalyticsController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.time.OffsetDateTime;
 import java.util.List;
 
 @RestController
@@ -22,54 +21,52 @@ import java.util.List;
 @RequestMapping("/analytics")
 public class AnalyticsController {
 
-    private final AnalyticsService analyticsService;
+        private final AnalyticsService analyticsService;
 
-    @Operation(summary = "Ver produccion por mes",
-                description = "Devuelve una lista de la suma de produccion por mes del ultimo año (por default) y se" +
+        @Operation(summary = "Ver produccion por mes", description = "Devuelve una lista de la suma de produccion por mes del ultimo año (por default) y se"
+                        +
                         "puede filtrar por fechas y producto")
-    @GetMapping("/monthly-production")
-    public ResponseEntity<List<MonthlyTotalDTO>> getMonthlyProduction(
-            @RequestParam(required = false) LocalDate startDate,
-            @RequestParam(required = false) LocalDate endDate,
-            @RequestParam(required = false) Long productId
-    ) {
-        List<MonthlyTotalDTO> dtoList = analyticsService.getMonthlyProduction(startDate, endDate, productId);
-        return new ResponseEntity<>(dtoList, HttpStatus.OK);
-    }
+        @GetMapping("/monthly-production")
+        public ResponseEntity<List<MonthlyTotalDTO>> getMonthlyProduction(
+                        @RequestParam(required = false) LocalDate startDate,
+                        @RequestParam(required = false) LocalDate endDate,
+                        @RequestParam(required = false) Long productId) {
+                List<MonthlyTotalDTO> dtoList = analyticsService.getMonthlyProduction(startDate, endDate, productId);
+                return new ResponseEntity<>(dtoList, HttpStatus.OK);
+        }
 
-    @Operation(summary = "Ver materiales consumidos por mes",
-            description = "Devuelve una lista de la suma de materiales consumidos por mes del ultimo año (por default) " +
-                    "y se puede filtrar por fechas y producto")
-    @GetMapping("/monthly-material-consumption")
-    public ResponseEntity<List<MonthlyTotalDTO>> getMonthlyMaterialConsumption(
-            @RequestParam(required = false) LocalDate startDate,
-            @RequestParam(required = false) LocalDate endDate,
-            @RequestParam(required = false) Long materialId
-    ) {
-        List<MonthlyTotalDTO> dtoList = analyticsService.getMonthlyMaterialConsumption(startDate, endDate, materialId);
-        return new ResponseEntity<>(dtoList, HttpStatus.OK);
-    }
+        @Operation(summary = "Ver materiales consumidos por mes", description = "Devuelve una lista de la suma de materiales consumidos por mes del ultimo año (por default) "
+                        +
+                        "y se puede filtrar por fechas y producto")
+        @GetMapping("/monthly-material-consumption")
+        public ResponseEntity<List<MonthlyTotalDTO>> getMonthlyMaterialConsumption(
+                        @RequestParam(required = false) LocalDate startDate,
+                        @RequestParam(required = false) LocalDate endDate,
+                        @RequestParam(required = false) Long materialId) {
+                List<MonthlyTotalDTO> dtoList = analyticsService.getMonthlyMaterialConsumption(startDate, endDate,
+                                materialId);
+                return new ResponseEntity<>(dtoList, HttpStatus.OK);
+        }
 
-    @Operation(summary = "Ver desperdicios por mes",
-            description = "Devuelve una lista de la suma de los desperdicios generados por mes del ultimo año (default)" +
-                    " y se puede filtrar por fechas y fase")
-    @GetMapping("/monthly-waste")
-    public ResponseEntity<List<MonthlyTotalDTO>> getMonthlyWaste(
-            @RequestParam(required = false) LocalDate startDate,
-            @RequestParam(required = false) LocalDate endDate,
-            @RequestParam(required = false) Phase phase,
-            @RequestParam(defaultValue = "false") boolean transferOnly
-    ) {
-        List<MonthlyTotalDTO> dtoList = analyticsService.getMonthlyWaste(startDate, endDate, phase, transferOnly);
-        return new ResponseEntity<>(dtoList, HttpStatus.OK);
-    }
+        @Operation(summary = "Ver desperdicios por mes", description = "Devuelve una lista de la suma de los desperdicios generados por mes del ultimo año (default)"
+                        +
+                        " y se puede filtrar por fechas y fase")
+        @GetMapping("/monthly-waste")
+        public ResponseEntity<List<MonthlyTotalDTO>> getMonthlyWaste(
+                        @RequestParam(required = false) LocalDate startDate,
+                        @RequestParam(required = false) LocalDate endDate,
+                        @RequestParam(required = false) Phase phase,
+                        @RequestParam(defaultValue = "false") boolean transferOnly) {
+                List<MonthlyTotalDTO> dtoList = analyticsService.getMonthlyWaste(startDate, endDate, phase,
+                                transferOnly);
+                return new ResponseEntity<>(dtoList, HttpStatus.OK);
+        }
 
-    @Operation(summary = "Ver resumen del ultimo mes",
-            description = "Devuelve diferentes datos relacionados al ultimo mes")
-    @GetMapping("/dashboard/monthly")
-    public ResponseEntity<DashboardStatsDTO> getMonthlyDashboard() {
-        DashboardStatsDTO dto =  analyticsService.getDashboardStats();
-        return new ResponseEntity<>(dto, HttpStatus.OK);
-    }
+        @Operation(summary = "Ver resumen del ultimo mes", description = "Devuelve diferentes datos relacionados al ultimo mes")
+        @GetMapping("/dashboard/monthly")
+        public ResponseEntity<DashboardStatsDTO> getMonthlyDashboard() {
+                DashboardStatsDTO dto = analyticsService.getDashboardStats();
+                return new ResponseEntity<>(dto, HttpStatus.OK);
+        }
 
 }
